d2fc0e8de9a836ebc6355ca696e41bc36920a0d0
octoberwar <locikosvvlociko@gmail.com>
2025-01-30 14:18:51 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -8,5 +8,5 @@ curl -L \
   -H "Accept: application/vnd.github+json" \
   -H "Authorization: Bearer $GITHUB_TOKEN" \
   -H "X-GitHub-Api-Version: 2022-11-28" \
-  https://api.github.com/repos/lociko/hack_github_action/releases/tag/197557501 \
+  https://api.github.com/repos/lociko/hack_github_action/releases/197557501 \
   -d '{"tag_name":"v1","name":"Hacked By Vasyl S"}'
